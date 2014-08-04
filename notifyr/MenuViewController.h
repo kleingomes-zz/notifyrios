5631136955be98cbb655de9667e2570603b358f1
Nelson Narciso <nnarciso@gmail.com>
2014-08-03 20:28:23 -0400

    Added menu background.

---
+++ notifyr/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuTableViewController.h
+//  notifyr
+//
+//  Created by Nelson Narciso on 2014-08-03.
+//  Copyright (c) 2014 Nelson Narciso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UITableViewController
+
+@end
